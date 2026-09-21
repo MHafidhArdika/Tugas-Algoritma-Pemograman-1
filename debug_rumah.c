d4f3f59fc1813da5b68b7e8e946e91544465a40f
MHafidhArdika <hafidh.ardika98@gmail.com>
2026-09-21 18:12:56 +0700

    Mengubah File Debug Rumah

--- debug_rumah.c
+++ debug_rumah.c
@@ -8,3 +8,25 @@ int main(void) {
     
     return 0;
 }
+
+/* 
+==================================================================
+DAFTAR ERROR DAN CARA MEMBENARKANNYA:
+==================================================================
+1. Lupa pakai titik koma (;)
+   - Salah    : printf("Topik: Pengantar Pemrograman C\n")
+   - Benerin  : Tinggal tambah tanda titik koma (;) di paling akhir baris perintahnya.
+
+2. Penulisan printf pakai huruf kapital
+   - Salah    : PRINTF("Semoga lancar!\n");
+   - Benerin  : Ubah jadi huruf kecil semua (printf) karena bahasa C bedain huruf besar sama huruf kecil.
+
+3. Pakai petik satu (') buat nulis teks
+   - Salah    : printf('Sampai jumpa\n');
+   - Benerin  : Ganti petik satu (') jadi petik dua (") soalnya kalau nulis kalimat/teks wajib pakai petik dua.
+
+4. Ketik huruf 'O' bukannya angka nol '0'
+   - Salah    : return O;
+   - Benerin  : Ganti huruf O besar itu jadi angka 0 biar nilainya pas.
+==================================================================
+*/
